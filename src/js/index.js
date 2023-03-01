4d35c4ebe2043295325a62d77bd06291845f06e0
Vladislav Bobyrev <perets1995@gmail.com>
2023-03-01 11:12:08 +0300

    :arrow_up: initial project

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+/**
+ * Функция normalize100vh устанавливает 100vh для доступного к просмотру вюпорта
+ * тем самым приводя для всех браузеров высоту к одному виду и адапптирует так
+ * что в браузерах с полосами которые сверху и снизу сайт не заходит под них
+ */
+function normalize100vh() {
+  const setHeight = () => {
+        const vh = window.innerHeight * 0.01 // наxодим  1vh
+
+        document.documentElement.style.setProperty('--vh', `${vh}px`)
+  // --vh использовать в css -> height: calc(var(--vh) * 100);
+}
+
+setHeight()
+window.addEventListener('resize', setHeight)
+}
+
+normalize100vh()
