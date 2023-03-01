792f639c78b4960439da73fa96a8406651f9561e
Vladislav Bobyrev <perets1995@gmail.com>
2023-03-01 12:26:54 +0300

    :wrench: set husky

--- src/js/index.js
+++ src/js/index.js
@@ -6,14 +6,14 @@
  */
 function normalize100vh() {
   const setHeight = () => {
-        const vh = window.innerHeight * 0.01 // наxодим  1vh
+    const vh = window.innerHeight * 0.01 // наxодим  1vh
 
-        document.documentElement.style.setProperty('--vh', `${vh}px`)
-  // --vh использовать в css -> height: calc(var(--vh) * 100);
-}
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+    // --vh использовать в css -> height: calc(var(--vh) * 100);
+  }
 
-setHeight()
-window.addEventListener('resize', setHeight)
+  setHeight()
+  window.addEventListener('resize', setHeight)
 }
 
 normalize100vh()
