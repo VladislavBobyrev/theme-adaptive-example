8872574a51d69292e0cfc3080635dacfcf22f4d7
Vladislav Bobyrev <perets1995@gmail.com>
2023-03-05 19:31:43 +0300

    Migrate from v4 in v8

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@
 function normalize100vh() {
   const setHeight = () => {
                 const vh = window.innerHeight * 0.01 // наxодим  1vh
-
+      
                 document.documentElement.style.setProperty('--vh', `${vh}px`)
                 // --vh использовать в css -> height: calc(var(--vh) * 100);
               }
